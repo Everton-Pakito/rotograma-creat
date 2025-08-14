6790168d51c9ac182c221e7ee7492df9c1025d92
EVERTON TEZZON FERREIRA <128308115+Everton-Pakito@users.noreply.github.com>
2025-08-13 21:27:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,18 @@
 const CACHE = 'rotograma-v1';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './app.js'];
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
       const copy = res.clone();
