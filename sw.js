0bc56602c6969fa077c09eca790f5c2b1079c7bc
EVERTON TEZZON FERREIRA <128308115+Everton-Pakito@users.noreply.github.com>
2025-08-13 21:46:03 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
-const CACHE = 'rotograma-v1';
-const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+const CACHE = 'rotograma-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.webmanifest'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
